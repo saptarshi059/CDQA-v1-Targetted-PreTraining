767ef9ce2ffcc0a7a317f6a46e76a1c28975584b
Saptarshi <ssengupta8@gmail.com>
2022-10-26 12:19:41 -0400

    New directory structure and PPL code

---
+++ src/Utils/convert_to_csv_corpus.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default="Saptarshi7/covid_qa_cleaned_CS", type=str)
+args = parser.parse_args()
+
+dataset = load_dataset(args.dataset, use_auth_token=True)
+
+contexts = list(set(dataset['train']['context']))
+
+pd.DataFrame(zip(list(range(len(contexts))), contexts)).to_csv(f"{args.dataset.replace('/','-')}_for_PPL_eval.csv", index=False)
---
+++ src/Utils/corpus_gen.py
@@ -0,0 +1,25 @@
+import wikipedia
+import pandas as pd
+from tqdm import tqdm
+import pickle5 as pickle
+
+'''
+s = pd.read_csv('Filtered_by_Stanza_NER.csv')
+s.drop(axis=1, inplace=True, columns=['Unnamed: 0'])
+s.drop_duplicates(inplace=True)
+'''
+
+stanza_ents = list(set(pickle.load(open('../data/stanza_ents.pkl', 'rb'))))
+texts = []
+ents = []
+
+#unique_entities = list(set(s.natural_text.to_list()))
+
+for ent in tqdm(stanza_ents):
+    try:
+        texts.append(wikipedia.page(wikipedia.search(str(ent), results=1)).content)
+        ents.append(str(ent))
+    except:
+        continue
+
+pd.DataFrame(zip(ents, texts), columns = ['ent' ,'text']).dropna().to_csv('corpus_only_stanza.csv', index=False)
