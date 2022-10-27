53742afffe25b80ef3796821797737caacac37e1
Saptarshi <ssengupta8@gmail.com>
2022-10-27 13:06:12 -0400

    New corpus gen code - more structured approach

--- src/Utils/Corpus_Generation.py
+++ src/Utils/Corpus_Generation.py
@@ -14,16 +14,18 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--model_checkpoint', default="distilbert-base-uncased", type=str)
 parser.add_argument('--corpus_name', default="mini_corpus-10T1CpT.csv", type=str)
+parser.add_argument('--stanza_ent_file', default="../../data/stanza_ents-from_questions.pkl", type=str)
 parser.add_argument('--num_of_ents', default=10, type=int)
 parser.add_argument('--num_of_ctx_per_ent', default=1, type=str)
 
 args = parser.parse_args()
 
-stanza_ents = pickle.load(open('../../data/stanza_ents.pkl', 'rb'))
+stanza_ents = pickle.load(open(args.stanza_ent_file, 'rb'))
 
 tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
 model_vocab = list(tokenizer.vocab.keys())
 
+# We only want to deal with those entities that are not in the model vocabulary otherwise we would get multiple embeddings for the same term.
 ent_in_model_vocab = []
 for ent in tqdm(stanza_ents):
     if ent in model_vocab:
