30c3cf75531637c34c628e263a733c599f3f650c
Saptarshi <ssengupta8@gmail.com>
2022-10-27 18:18:53 -0400

    all_run script

--- all_run.sh
+++ all_run.sh
@@ -1,3 +1,5 @@
+export CUDA_VISIBLE_DEVICES=2
+
 #With new tokens - 10T1CpT
 
 accelerate launch src/FineTuning_Scripts/Fine-Tuning_for_MLM.py --model_checkpoint distilbert-base-uncased --trained_model_name distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T1CpT --corpus_file data/our-wikipedia-corpus/Tokens_From_Question_side/mini_corpus-10T1CpT.csv --use_new_tokens True
