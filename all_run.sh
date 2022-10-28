e9f72fcbffdcc7824493704be41e9ee709d4e054
Saptarshi <ssengupta8@gmail.com>
2022-10-28 15:28:23 -0400

    all_run

--- all_run.sh
+++ all_run.sh
@@ -13,20 +13,20 @@ accelerate launch src/FineTuning_Scripts/covidqa_ft.py --model_checkpoint distil
 
 #without new tokens - 10T5CpT
 
-accelerate launch src/FineTuning_Scripts/Fine-Tuning_for_MLM.py --model_checkpoint distilbert-base-uncased --trained_model_name distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT --corpus_file data/our-wikipedia-corpus/Tokens_From_Question_side/mini_corpus-10T5CpT.csv --use_new_tokens False
+#accelerate launch src/FineTuning_Scripts/Fine-Tuning_for_MLM.py --model_checkpoint distilbert-base-uncased --trained_model_name distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT --corpus_file data/our-wikipedia-corpus/Tokens_From_Question_side/mini_corpus-10T5CpT.csv --use_new_tokens False
 
-accelerate launch src/Utils/ppl.py --model_checkpoint distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT --corpus_file src/Utils/Saptarshi7-covid_qa_cleaned_CS_for_PPL_eval.csv
+#accelerate launch src/Utils/ppl.py --model_checkpoint distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT --corpus_file src/Utils/Saptarshi7-covid_qa_cleaned_CS_for_PPL_eval.csv
 
 accelerate launch ../generalization-hypothesis/src/model_analysis/squad_ft.py --model_checkpoint distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT --trained_model_name distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT-squad
 
-accelerate launch src/FineTuning_Scripts/covidqa_ft.py --model_checkpoint distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT-squad --trained_model_name distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT-squad-covidqa
+accelerate launch --main_process_port 80 src/FineTuning_Scripts/covidqa_ft.py --model_checkpoint distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT-squad --trained_model_name distilbert-base-uncased-extended-PT-wo-new-tokens-mini_corpus-10T5CpT-squad-covidqa
 
 
 #with new tokens - 10T5CpT
 
-accelerate launch src/FineTuning_Scripts/Fine-Tuning_for_MLM.py --model_checkpoint distilbert-base-uncased --trained_model_name distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT --corpus_file data/our-wikipedia-corpus/Tokens_From_Question_side/mini_corpus-10T5CpT.csv --use_new_tokens True
+#accelerate launch src/FineTuning_Scripts/Fine-Tuning_for_MLM.py --model_checkpoint distilbert-base-uncased --trained_model_name distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT --corpus_file data/our-wikipedia-corpus/Tokens_From_Question_side/mini_corpus-10T5CpT.csv --use_new_tokens True
 
-accelerate launch src/Utils/ppl.py --model_checkpoint distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT --corpus_file src/Utils/Saptarshi7-covid_qa_cleaned_CS_for_PPL_eval.csv
+#accelerate launch src/Utils/ppl.py --model_checkpoint distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT --corpus_file src/Utils/Saptarshi7-covid_qa_cleaned_CS_for_PPL_eval.csv
 
 accelerate launch ../generalization-hypothesis/src/model_analysis/squad_ft.py --model_checkpoint distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT --trained_model_name distilbert-base-uncased-extended-PT-with-new-tokens-mini_corpus-10T5CpT-squad
 
