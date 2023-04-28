14c30b9956a8971a7e8e6fa29662403c9fc4f973
Saptarshi <ssengupta8@gmail.com>
2023-04-28 16:21:41 -0400

    galactica-baseline.py

--- src/FineTuning_Scripts/old_code/squad_ft.py
+++ src/FineTuning_Scripts/old_code/squad_ft.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoConfig, logging, default_data_collator, \
-    get_scheduler, set_seed
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoConfig, logging, default_data_collator, get_scheduler, set_seed
 from datasets import load_dataset, load_metric
 from torch.utils.data import DataLoader
 from accelerate import Accelerator
@@ -15,7 +14,6 @@ import argparse
 import random
 import torch
 
-
 def str2bool(v):
     if isinstance(v, bool):
         return v
@@ -27,7 +25,6 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-
 
 def prepare_train_features(examples):
     # Some of the questions have lots of whitespace on the left, which is not useful and will make the
@@ -106,7 +103,6 @@ def prepare_train_features(examples):
 
     return tokenized_examples
 
-
 def prepare_validation_features(examples):
     # Some of the questions have lots of whitespace on the left, which is not useful and will make the
     # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
@@ -152,7 +148,6 @@ def prepare_validation_features(examples):
 
     return tokenized_examples
 
-
 def compute_metrics(start_logits, end_logits, features, examples):
     example_to_features = collections.defaultdict(list)
     for idx, feature in enumerate(features):
@@ -170,8 +165,8 @@ def compute_metrics(start_logits, end_logits, features, examples):
             end_logit = end_logits[feature_index]
             offsets = features[feature_index]["offset_mapping"]
 
-            start_indexes = np.argsort(start_logit)[-1: -n_best - 1: -1].tolist()
-            end_indexes = np.argsort(end_logit)[-1: -n_best - 1: -1].tolist()
+            start_indexes = np.argsort(start_logit)[-1 : -n_best - 1 : -1].tolist()
+            end_indexes = np.argsort(end_logit)[-1 : -n_best - 1 : -1].tolist()
             for start_index in start_indexes:
                 for end_index in end_indexes:
                     # Skip answers that are not fully in the context
@@ -179,13 +174,13 @@ def compute_metrics(start_logits, end_logits, features, examples):
                         continue
                     # Skip answers with a length that is either < 0 or > max_answer_length
                     if (
-                            end_index < start_index
-                            or end_index - start_index + 1 > max_answer_length
+                        end_index < start_index
+                        or end_index - start_index + 1 > max_answer_length
                     ):
                         continue
 
                     answer = {
-                        "text": context[offsets[start_index][0]: offsets[end_index][1]],
+                        "text": context[offsets[start_index][0] : offsets[end_index][1]],
                         "logit_score": start_logit[start_index] + end_logit[end_index],
                     }
                     answers.append(answer)
@@ -202,12 +197,10 @@ def compute_metrics(start_logits, end_logits, features, examples):
     theoretical_answers = [{"id": ex["id"], "answers": ex["answers"]} for ex in examples]
     return metric.compute(predictions=predicted_answers, references=theoretical_answers)
 
-
 def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2 ** 32
+    worker_seed = torch.initial_seed() % 2**32
     np.random.seed(worker_seed)
     random.seed(worker_seed)
-
 
 parser = argparse.ArgumentParser()
 
@@ -225,6 +218,7 @@ parser.add_argument('--max_answer_length', default=30, type=int)
 parser.add_argument('--trial_mode', default=False, type=str2bool)
 parser.add_argument('--random_state', default=42, type=int)
 parser.add_argument('--freeze_PT_layers', default=False, type=str2bool)
+
 
 args = parser.parse_args()
 
@@ -247,15 +241,14 @@ accelerator = Accelerator()
 device = accelerator.device
 
 if model_checkpoint == 'studio-ousia/luke-base':
-    tokenizer = AutoTokenizer.from_pretrained(
-        'roberta-base')  # since luke doesn't have a fast implementation & it has the same vocab as roberta
+    tokenizer = AutoTokenizer.from_pretrained('roberta-base') #since luke doesn't have a fast implementation & it has the same vocab as roberta
 else:
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
 data_collator = default_data_collator
 
-max_length = args.max_length  # The maximum length of a feature (question and context)
-doc_stride = args.stride  # The authorized overlap between two part of the context when splitting it is needed.
+max_length = args.max_length # The maximum length of a feature (question and context)
+doc_stride = args.stride # The authorized overlap between two part of the context when splitting it is needed.
 max_answer_length = args.max_answer_length
 n_best = args.n_best
 
@@ -263,29 +256,22 @@ pad_on_right = tokenizer.padding_side == "right"
 
 if args.trial_mode == True:
     print('Running Code in Trial Mode to see if everything works properly...')
-    raw_datasets = load_dataset("squad_v2" if squad_v2 else "squad",
-                                split=['train[:160]', 'validation[:10]'])  # Testing purposes
-    train_dataset = raw_datasets[0].map(prepare_train_features, batched=True,
-                                        remove_columns=raw_datasets[0].column_names)
-    validation_dataset = raw_datasets[1].map(prepare_validation_features, batched=True,
-                                             remove_columns=raw_datasets[1].column_names)
+    raw_datasets = load_dataset("squad_v2" if squad_v2 else "squad", split=['train[:160]','validation[:10]']) #Testing purposes
+    train_dataset = raw_datasets[0].map(prepare_train_features, batched=True, remove_columns=raw_datasets[0].column_names)
+    validation_dataset = raw_datasets[1].map(prepare_validation_features, batched=True, remove_columns=raw_datasets[1].column_names)
 else:
     raw_datasets = load_dataset("squad_v2" if squad_v2 else "squad")
-    train_dataset = raw_datasets['train'].map(prepare_train_features, batched=True,
-                                              remove_columns=raw_datasets['train'].column_names)
-    validation_dataset = raw_datasets['validation'].map(prepare_validation_features, batched=True,
-                                                        remove_columns=raw_datasets['validation'].column_names)
+    train_dataset = raw_datasets['train'].map(prepare_train_features, batched=True, remove_columns=raw_datasets['train'].column_names)
+    validation_dataset = raw_datasets['validation'].map(prepare_validation_features, batched=True, remove_columns=raw_datasets['validation'].column_names)
 
-metric = load("squad_v2")
+metric = load("squad")
 
 train_dataset.set_format("torch")
-train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=data_collator, batch_size=batch_size,
-                              worker_init_fn=seed_worker, generator=g)
+train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=data_collator, batch_size=batch_size, worker_init_fn=seed_worker, generator=g)
 
 validation_set = validation_dataset.remove_columns(["example_id", "offset_mapping"])
 validation_set.set_format("torch")
-eval_dataloader = DataLoader(validation_set, collate_fn=data_collator, batch_size=batch_size,
-                             worker_init_fn=seed_worker, generator=g)
+eval_dataloader = DataLoader(validation_set, collate_fn=data_collator, batch_size=batch_size, worker_init_fn=seed_worker, generator=g)
 
 model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
 output_dir = args.trained_model_name
@@ -298,8 +284,7 @@ if args.freeze_PT_layers == True:
 
 optimizer = AdamW(model.parameters(), lr=args.learning_rate)
 
-model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(model, optimizer, train_dataloader,
-                                                                          eval_dataloader)
+model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(model, optimizer, train_dataloader, eval_dataloader)
 
 num_train_epochs = args.epochs
 num_update_steps_per_epoch = len(train_dataloader)
@@ -352,4 +337,4 @@ for epoch in range(num_train_epochs):
     unwrapped_model.save_pretrained(output_dir, save_function=accelerator.save)
     if accelerator.is_main_process:
         tokenizer.save_pretrained(output_dir)
-        # repo.push_to_hub(commit_message=f"Training in progress epoch {epoch}", blocking=False)
+        #repo.push_to_hub(commit_message=f"Training in progress epoch {epoch}", blocking=False)
