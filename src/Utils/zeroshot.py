99289a3eaadcc03519763c420b055658c770c56f
Saptarshi <ssengupta8@gmail.com>
2023-05-10 16:50:24 -0400

    galactica-baseline.py

--- src/Utils/zeroshot.py
+++ src/Utils/zeroshot.py
@@ -36,7 +36,7 @@ class QADataset(Dataset):
             self.dataset = load_dataset('squad_v2')
         else:
             self.dataset = DatasetDict({"validation": load_dataset('json', data_files=os.path.abspath(
-                '../../../data/RadQA/radqa-a-question-answering'
+                '../../data/RadQA/radqa-a-question-answering'
                 '-dataset-to-improve-comprehension-of'
                 '-radiology-reports-1.0.0/test.jsonl'))['train']})
 
