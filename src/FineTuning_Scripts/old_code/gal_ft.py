98b047f0f4f5a5a71d4b3a69ebd798feaf486f62
Saptarshi <ssengupta8@gmail.com>
2023-07-06 16:50:15 -0400

    galactica-baseline.py

--- src/FineTuning_Scripts/old_code/gal_ft.py
+++ src/FineTuning_Scripts/old_code/gal_ft.py
@@ -147,9 +147,9 @@ for epoch in range(num_train_epochs):
     accelerator.print("Evaluation!")
     predicted_tensors = []
     for step, batch in tqdm(enumerate(eval_dataloader)):
-        #with torch.no_grad():
+        with torch.no_grad():
         #    fsdp_wrapped_gal.forward(input_ids=batch['input_ids'])
-        with FSDP.summon_full_params(fsdp_wrapped_gal, recurse=False):
+        #with FSDP.summon_full_params(fsdp_wrapped_gal, recurse=False):
             predicted_tensors.extend(fsdp_wrapped_gal.generate(input_ids=batch['input_ids'],
                                                                attention_mask=batch['attention_mask'],
                                                                max_new_tokens=30))
