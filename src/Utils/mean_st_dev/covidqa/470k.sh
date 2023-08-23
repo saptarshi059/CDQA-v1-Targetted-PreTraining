08b5264b715e894cfd9d6b40701e39ae5c7c1c03
Saptarshi <ssengupta8@gmail.com>
2023-08-23 16:42:05 -0400

    galactica-baseline.py

--- src/Utils/mean_st_dev/covidqa/470k.sh
+++ src/Utils/mean_st_dev/covidqa/470k.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#SBATCH --nodes=1                   # Number of nodes to request
+#SBATCH --cpus-per-task=4           # Number of CPUs per node to request
+#SBATCH --gpus-per-node=1           # Number of GPUs per node to request
+#SBATCH --job-name="on 470k ft"   	# A nice readable name of your job, to see it in the queue, instead of numbers
+#SBATCH --output=jobName.%J.out     # Store the output console text to a file called jobName.<assigned job number>.out
+#SBATCH --error=jobName.%J.err      # Store the error messages to a file called jobName.<assigned job number>.err
+#SBATCH --mail-type=FAIL,BEGIN,END  # Send an email when the job starts, ends, or fails
+#SBATCH --mail-user=saptarshi.sengupta@l3s.de # Email address to send the email to
+
 # 470k trial
 a=0
 while [ $a -lt 5 ]
