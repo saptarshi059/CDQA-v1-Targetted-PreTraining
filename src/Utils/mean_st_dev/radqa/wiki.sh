bf49d754be8f4f8e2661da34b71450688029daf9
Saptarshi <ssengupta8@gmail.com>
2023-11-06 14:43:05 -0500

    updated FT_MLM to handle absolute paths

--- src/Utils/mean_st_dev/radqa/wiki.sh
+++ src/Utils/mean_st_dev/radqa/wiki.sh
@@ -16,7 +16,7 @@ do
   "../../../FineTuning_Scripts/Fine-Tuning_for_MLM.py" \
   --model_checkpoint "bert-base-cased" \
   --trained_model_name "bert-base-wiki" \
-  --training_corpus "../../../../data/RadQA/wiki_corpus_radqa.parquet" \
+  --training_corpus "../../../../data/RadQA/radqa-corpora/wiki_corpus_radqa.parquet" \
   --eval_corpus "../../../../data/RadQA/RadQA_for_PPL_eval.csv" --epochs 3
 
   accelerate launch --main_process_port 12580 --mixed_precision fp16 \
